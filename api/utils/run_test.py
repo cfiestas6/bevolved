fb5cbcdf25256deb73c489bb996dfe36b07ddac4
Noeljarillo <noel.jarillo@gmail.com>
2023-07-08 13:16:26 -0700

    fixed api return

--- api/utils/run_test.py
+++ api/utils/run_test.py
@@ -8,7 +8,8 @@ def run_tests_on_repo(repo_url, test_path):
     repo_name = repo_url.split('/')[-1].split('.')[0]  # get repo name from url
     g = git.cmd.Git(os.getcwd())
     g.clone(repo_url)
-
+    count = 0
+    total = 0
     # Walk through the cloned repo directory
     for dirpath, _, filenames in os.walk(repo_name):
         for file in filenames:
@@ -22,18 +23,20 @@ def run_tests_on_repo(repo_url, test_path):
                 test_main_file_path = os.path.join(test_path, relative_dirpath, "main.c").replace("\\", "/")
                 
                 print(f"Expected test file location: {test_main_file_path}")  # For debugging
-                
+                total = 0
                 # Copy main.c (test file) to source directory
                 try:
                     shutil.copy(test_main_file_path, dirpath)
                 except FileNotFoundError:
                     print(f"Test file for {function_name} not found.")
                     continue
-                
+                total += 1
                 # Compile and run tests
                 try:
                     subprocess.run(['gcc', '-o', function_name, src_file_path, os.path.join(dirpath, 'main.c')], check=True)
                     subprocess.run(['./' + function_name], check=True)
                     print(f"Test for {function_name} passed successfully.")
+                    count += 1
                 except subprocess.CalledProcessError:
                     print(f"Test for {function_name} failed.")
+                return((count/total)*100)
--- page/my-app/pages/index.js
+++ page/my-app/pages/index.js
@@ -1,102 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import Web3 from 'web3';
-import detectEthereumProvider from '@metamask/detect-provider';
+
+
+import { ethers } from 'ethers';
+import { useState, useEffect } from 'react';
 import CONTRACT_ABI from "./abi.json";
 
 const CONTRACT_ADDRESS = '0x4648a43B2C14Da09FdF82B161150d3F634f40491';
 
-export default function ExamPage() {
-  const [web3, setWeb3] = useState(null);
+export default function Home() {
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [account, setAccount] = useState(null);
   const [contract, setContract] = useState(null);
-  const [accounts, setAccounts] = useState([]);
-  const [studentId, setStudentId] = useState('');
-  const [studentIdExam, setStudentIdExam] = useState('');
-  const [studentIdGrade, setStudentIdGrade] = useState('');
-  const [verifierAddress, setVerifierAddress] = useState('');
-  const [examId, setExamId] = useState('');
-  const [examIdPass, setExamIdPass] = useState('');
-  const [nonce, setNonce] = useState('');
-  const [isPassed, setIsPassed] = useState(false);
-  const [course, setCourse] = useState('');
-  const [grade, setGrade] = useState('');
+  const [studentId, setStudentId] = useState(1);
+  const [examId, setExamId] = useState(1);
+  const [examPrice, setExamPrice] = useState(1);
 
   useEffect(() => {
-    async function initWeb3() {
-      // Here, we're using MetaMask's provider detection utility to get the provider.
-      const provider = await detectEthereumProvider();
-      
-      if (provider) {
-        const web3Instance = new Web3(provider);
-        const contractInstance = new web3Instance.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
-        setWeb3(web3Instance);
-        setContract(contractInstance);
+      if (typeof window.ethereum !== 'undefined') {
+          const ethereumProvider = new ethers.providers.Web3Provider(window.ethereum);
+          setProvider(ethereumProvider);
 
-        const accounts = await web3Instance.eth.getAccounts();
-        setAccounts(accounts);
-        
-        // Request account access if needed (This part requires user interaction.)
-        if (provider !== window.ethereum) {
-          console.error('Do you have multiple wallets installed?');
-        } else if (!window.ethereum.isConnected()) {
-          await window.ethereum.request({ method: 'eth_requestAccounts' });
-        }
+          const signer = ethereumProvider.getSigner();
+          setSigner(signer);
 
-        const chainId = await web3Instance.eth.getChainId();
-          console.log('Chain ID:', chainId);  // Check the chain ID in console
-
-          if (chainId !== 5) {
-            alert('Please connect to the Goerli test network');
-          }
-      } else {
-        alert('Please install MetaMask or another Ethereum wallet');
+          const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+          setContract(contract);
       }
-    }
-    
-    initWeb3();
-}, []);
+  }, []);
 
-  async function goExam() {
-    if (!contract || !web3) return;
+  useEffect(() => {
+      const getAccount = async () => {
+          if (signer) {
+              const account = await signer.getAddress();
+              setAccount(account);
+          }
+      };
+      getAccount();
+  }, [signer]);
 
-    await contract.methods.go_exam(studentId, verifierAddress, examId, nonce).send({
-      from: accounts[0],
-      value: web3.utils.toWei('1', 'ether')
-    });
-  }
-
-  async function passExam() {
-    if (!contract) return;
-
-    await contract.methods.passExam(studentIdExam, examIdPass, isPassed).send({
-      from: accounts[0]
-    });
-  }
-
-  async function addGrade() {
-    if (!contract) return;
-
-    await contract.methods.addGrade(studentIdGrade, course, grade).send({
-      from: accounts[0]
-    });
-  }
+  const handleGoExam = async () => {
+      try {
+          const nonce = await provider.getTransactionCount(account);
+          const gasPrice = await provider.getGasPrice();
+          const gasEstimate = await contract.estimateGas.go_exam(studentId, account, examId, nonce, { value: ethers.utils.parseEther(examPrice.toString()) });
+          const transactionResponse = await contract.go_exam(studentId, account, examId, nonce, {
+              value: ethers.utils.parseEther(examPrice.toString()),
+              gasLimit: gasEstimate.toNumber(),
+              gasPrice: gasPrice
+          });
+          console.log('Transaction Hash:', transactionResponse.hash);
+          const transactionReceipt = await transactionResponse.wait();
+          console.log('Transaction Receipt:', transactionReceipt);
+      } catch (error) {
+          console.log('Error:', error);
+      }
+  };
 
   return (
-    <div>
-      <input value={studentId} onChange={(e) => setStudentId(e.target.value)} placeholder="Student ID" />
-      <input value={verifierAddress} onChange={(e) => setVerifierAddress(e.target.value)} placeholder="Verifier Address" />
-      <input value={examId} onChange={(e) => setExamId(e.target.value)} placeholder="Exam ID" />
-      <input value={nonce} onChange={(e) => setNonce(e.target.value)} placeholder="Nonce" />
-      <button onClick={goExam}>Go Exam</button>
-
-      <input value={studentIdExam} onChange={(e) => setStudentIdExam(e.target.value)} placeholder="Student ID" />
-      <input value={examIdPass} onChange={(e) => setExamIdPass(e.target.value)} placeholder="Exam ID" />
-      <input type="checkbox" checked={isPassed} onChange={(e) => setIsPassed(e.target.checked)} />
-      <button onClick={passExam}>Pass Exam</button>
-
-      <input value={studentIdGrade} onChange={(e) => setStudentIdGrade(e.target.value)} placeholder="Student ID" />
-      <input value={course} onChange={(e) => setCourse(e.target.value)} placeholder="Course" />
-      <input value={grade} onChange={(e) => setGrade(e.target.value)} placeholder="Grade" />
-      <button onClick={addGrade}>Add Grade</button>
-    </div>
+      <div>
+          <button onClick={handleGoExam}>Go Exam</button>
+      </div>
   );
 }
