3788ec86f60319a6f27ba9477deffd1012d5d372
Carlos Fiestas <cfiestas6@gmail.com>
2023-07-09 02:32:26 +0200

    wagmi

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,18 +1,19 @@
 import { Footer, Join, Navigation } from '@/devlink';
 import React, { useEffect, useState } from 'react';
-import { ethers } from "ethers";
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 import Head from 'next/head';
 
-export default function Signup(): JSX.Element {
-    const copyYear = new Date().getFullYear();
+export default function Signup(): any {
+    const { address } = useAccount()
+    const { connect } = useConnect({
+        connector: new InjectedConnector(),
+    })
+  const { disconnect } = useDisconnect()
 
-    const [wallet, setWallet] = useState<string>("Connect Wallet");
-
-    useEffect(() => {
-        function connectWallet() {
-        }
-    }, []);
-
+  if (address) {
+    alert("Your address is " + address)
+  }
     return (
         <>
             <Head>
@@ -23,13 +24,14 @@ export default function Signup(): JSX.Element {
             </Head>
             <Navigation
                 coursesUrl={{ href: "/courses" }}
-                connectWallerUrl={{ onClick: () => (alert('asfdg')) }}
                 homeUrl={{ href: "/" }} />
             <Join
                 formProps={{
                     onSubmit: async (e: React.FormEvent<HTMLFormElement>) => {
                         e.preventDefault();
                         
+                        // Connect to web3 provider usiing wagmi
+                        connect();
                         // convert the form data to JSON
                         const formData = new FormData(e.target);
                         const data = Object.fromEntries(formData.entries());
@@ -38,7 +40,7 @@ export default function Signup(): JSX.Element {
                         await fetch("http://127.0.0.1:5000/users/signup", {
                             body: JSON.stringify({
                                 email: data.email,
-                                wallet: "0x6b81844B6633Ad23asdfgasdfEFE4261893cb2d",
+                                wallet: address,
                                 first_name: data.first_name,
                                 last_name: data.last_name
                             }),
